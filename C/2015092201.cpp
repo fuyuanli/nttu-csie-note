87a0c036692f99dd462c5ed60e24524a7bbfc6b1
Your Name <you@example.com>
2015-09-22 17:10:03 +0800

    20150922

---
+++ C/2015092201.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main (){
+	float bcc=60.0, clang=80.0, cal=90.0, acc=98.0, eco=65.0;
+	float average = (bcc+clang+cal+acc+eco)/5.0;
+	
+	printf("%f \n",bcc+clang+cal+acc+eco);
+	printf("%f \n",average);
+	return 0;
+}
