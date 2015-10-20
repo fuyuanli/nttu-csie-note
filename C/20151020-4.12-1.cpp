c98ee87d68914c79c143c99926cc1cd319c60fda
FuYuan <fuyuanli0625@#gmail.com>
2015-10-20 17:12:53 +0800

    20151020

---
+++ C/20151020-4.12-1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main () {
+	int i=2, j=3;
+	double k;
+	
+	k = 2/3;
+	printf("2/3 = %f\n",k);
+	
+	k = 2/3.;
+	printf("2/3 = %f\n",k);
+	
+	system("pause");
+	return 0;	
+}
---
+++ C/20151020-if.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+	
+	int num = 50;
+	
+	if (50<=num & num<60)
+		printf("Down\n");
+		
+	system("pause");
+	return 0;
+
+}
