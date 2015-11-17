4a9a46c5179a721151eda57e86368f0d03016a75
fuyuanli <fuyuanli0625@gmail.com>
2015-11-17 15:43:49 +0800

    Create 20151117-1.cpp

---
+++ C/20151117-1.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main (){
+  int n=100,i=0,sum=0;
+	printf("n = %d , 1+2+3+...+%d+%d = ?\n",n,n-1,n);
+	
+	//FOR LOOP	
+	for(i=1;i<=n;i++){
+		sum+=i;
+	}
+	printf("For 迴圈 : %d\n",sum);
+	
+	i=1;
+	sum=0;
+	
+	//WHILE LOOP
+	while(i<=n){
+		sum+=i;
+		++i;
+	}
+	printf("While 迴圈 : %d\n",sum);
+	
+	return 0;
+}
