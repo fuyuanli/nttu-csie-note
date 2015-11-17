49a2bc7d5f9ba574493ccca656b852261a7175ba
fuyuanli <fuyuanli0625@gmail.com>
2015-11-17 17:40:17 +0800

    20151117

---
+++ C/20151117-04.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	
+	int n=12,count=0;
+	
+	//Count how many factor 2's does n have?
+	while(n>=1){
+		
+		if(n%2==0)
+			++count;
+			
+		n = n/2;
+	}
+	printf("%d",count);
+}
