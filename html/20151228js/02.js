f70a1428145b425e2b405e72ce3addfd41c9ffc0
FuYuan Li <fuyuanli0625@gmail.com>
2015-12-28 16:37:06 +0800

    期末考

--- html/20151228js/02.js
+++ html/20151228js/02.js
@@ -26,3 +26,38 @@ $(document).ready(function(){
 	});
 });
 
+$(document).ready(function(){
+	$("button").click(function(){
+		$("p").append("hellow"); 
+		//在段落p後面都加上一串新段落
+	});
+});
+
+$(document).ready(function(){
+	$("button").click(function(){
+		$("p#demo").remove(); 
+		//移除段落p ID demo
+	});
+});
+
+$(document).ready(function(){
+	$("button").click(function(){
+		$("p#demo").empty(); 
+		//清空段落p id demo
+	});
+});
+
+$(document).ready(function(){
+	$("button").click(function(){
+		$("div#demo p").css("color","red"); 
+		//把id為demo的div 內的p文字變成紅色
+	});
+});
+
+$(document).ready(function(){
+	$("button").click(function(){
+		$("div:first p").last().css("color","red"); 
+		//把第一個div 內的p文字變為紅色
+	});
+});
+
