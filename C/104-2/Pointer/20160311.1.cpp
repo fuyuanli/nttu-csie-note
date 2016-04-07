bbd7f0a887e3c729d2c0214d58df03373667f6d1
FuYuan Li <fuyuanli0625@gmail.com>
2016-04-08 01:43:31 +0800

    add code

--- C/104-2/Pointer/20160311.1.cpp
+++ C/104-2/Pointer/20160311.1.cpp
@@ -3,18 +3,18 @@
 int *max_loc(int* a,int n){
 	int *mp=a;
 	int *p=a;
-	
+
 	for(int i=1;i<n;++i)
 		if(*(p+i) > *mp)
-			mp=p+i;
+			mp=p+i; // *mp=*(p+i);
 			//mp=mp+1;
 	return mp;
-	
+
 }
 
 int main(){
 	int a[]={3,5,7,15,1};
 	printf("%d",*max_loc(a,5));
-	
+
 	return 0;
 }
