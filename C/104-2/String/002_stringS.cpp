2a384999e2104c8320ada255484ecf1535b54aa7
FuYuan Li <fuyuanli0625@gmail.com>
2016-04-15 15:21:36 +0800

    repaired

--- C/104-2/String/002_stringS.cpp
+++ C/104-2/String/002_stringS.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 // Does string s contain char C ?
-bool strlen(const char *s){
+bool strlen(const char *s,char c){
     for (; *s != 0; ++s)
-        if(*s=='C')
+        if(*s==c)
             return 1;
     return 0;
 }
 
 int main(){
     const char *s = "abC";
-    printf("%s", strlen(s)?"1":"0");
+    printf("%s", strlen(s,'C')?"1":"0");
 }
