5a32f15938382d71f652513de36d769a640360f6
FuYuan Li <fuyuanli0625@gmail.com>
2016-04-15 15:48:35 +0800

    repaired

--- C/104-2/String/003_string.cpp
+++ C/104-2/String/003_string.cpp
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
 // Arr 2 strings s1 and s2 equal?
+int strlen(const char *s){
+    int len = 0;
+    for (; *s != 0; ++s)
+        ++len;
+    return len;
+}
+
 bool strequal(const char *s1,const char *s2){
-    for(int i=0;;++i)
+    if(strlen(s1)!=strlen(s2))
+        return 0;
+
+    for(int i=0;i<strlen(s1);++i)
         if(*(s1+i) != *(s2+i))
             return 0;
     return 1;
